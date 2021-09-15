58904dd5ca24cffbdb6e03a3b494468a537229a1
finesaaa <indriatifiqey16@gmail.com>
2021-09-15 15:26:30 +0700

    feat: draw a triangle

--- main.js
+++ main.js
@@ -1,36 +1,96 @@
-function main(){
-    var canvas = document.getElementById("myCanvas");
-    var context = canvas.getContext("webgl");
+function main() {
+  var canvas = document.getElementById("myCanvas");
+  var gl = canvas.getContext("webgl");
 
-    var vertexShaderCode = `
-    void main(){
-        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-        gl_PointSize = 10.0;
+  // Define vertcies data
+  /*
+   * A (-0.5, -0.5)
+   * B (0.5, -0.5)
+   * C (0.0, 0.5)
+   */
+
+  var vertices = [
+    -0.5, -0.5, 0.0, 1.0, 0.0,     // Point A
+     0.5, -0.5, 0.0, 0.0, 1.0,     // Point B
+     0.5, -0.5, 0.0, 0.0, 1.0,     // Point C
+     0.0,  0.5, 1.0, 0.0, 0.0,     // Point D
+     0.0,  0.5, 1.0, 0.0, 0.0,     // Point E
+    -0.5, -0.5, 0.0, 1.0, 0.0      // Point F
+  ];
+
+
+  // Create a lnekkd-list for storing the vertices data
+  var buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+  // var vertexShaderCode = document.getElementById("vertexShaderCode").text;
+  var vertexShaderCode = `
+    attribute vec2 aPosition;
+    attribute vec3 aColor;
+    varying vec3 vColor;
+    void main() { 
+        gl_Position = vec4(aPosition, 0.0, 1.0); // Center of the coordnate
+        gl_PointSize = 10.0; 
+        vColor = aColor;
     }
     `;
 
-    var vertexShader = context.createShader(context.VERTEX_SHADER);
-    context.shaderSource(vertexShader, vertexShaderCode);
-    context.compileShader(vertexShader);
-
-    var fragmentShaderCode = `
+  var fragmentShaderCode = `
+    precision mediump float;
+    varying vec3 vColor;
     void main(){
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        gl_FragColor = vec4(vColor, 1.0);
     }
     `;
 
-    var fragmentShader = context.createShader(context.FRAGMENT_SHADER);
-    context.shaderSource(fragmentShader, fragmentShaderCode);
-    context.compileShader(fragmentShader);
+  // Create .c in GPU
+  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShader, vertexShaderCode);
+  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShader, fragmentShaderCode);
 
-    var shaderProgram = context.createProgram();
-    context.attachShader(shaderProgram, vertexShader);
-    context.attachShader(shaderProgram, fragmentShader);
-    context.linkProgram(shaderProgram);
-    context.useProgram(shaderProgram);
+  // Compile .c into .o
+  gl.compileShader(vertexShader);
+  gl.compileShader(fragmentShader);
 
-    context.clearColor(1.0, 1.0, 0.0, 1.0);
-    context.clear(context.COLOR_BUFFER_BIT);
+  // Prepare a .exe shell (shader program)
+  var shaderProgram = gl.createProgram();
 
-    context.drawArrays(context.POINTS, 0, 1);
+  // Put the two .o files into the shell
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+
+  // Link the two .o files, so together they can be a runnable program/context.
+  gl.linkProgram(shaderProgram);
+
+  // Start using the context (analogy: start using the paints and the brushes)
+  gl.useProgram(shaderProgram);
+
+  // Teach the computer how to collect
+  //  the positional values from ARRAY_BUFFER
+  //  to each vertex being processed
+  var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 0);
+  gl.enableVertexAttribArray(aPosition);
+
+  var aColor = gl.getAttribLocation(shaderProgram, "aColor");
+  gl.vertexAttribPointer(
+    aColor,
+    3,
+    gl.FLOAT,
+    false,
+    5 * Float32Array.BYTES_PER_ELEMENT,
+    2 * Float32Array.BYTES_PER_ELEMENT
+  );
+  gl.enableVertexAttribArray(aColor);
+
+  // Change screen to dark grey
+  gl.clearColor(0.1, 0.1, 0.1, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Tell that we've three vertices
+  gl.drawArrays(gl.LINES, 0, 6);
 }
+
+window.onload = main;
